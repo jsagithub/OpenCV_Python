abfafec0bfbd5d88ededc911f4aa9fd772ae84b9
jsagithub <jsa_epe@hotmail.com>
2020-01-29 14:15:08 +0000

    use keybord keys to make something

--- get_started_with_images.py
+++ get_started_with_images.py
@@ -1,11 +1,11 @@
 import cv2
 
-img = cv2.imread('lena.jpg', 0)
-
-print(img)
+img = cv2.imread('lena.jpg', -1)
 
 cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imwrite('lena_copy.png') 
+k = cv2.waitKey(0)
+if k==27: /*escape key*/
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('lena_copy.png') 
+    cv2.destroyAllWindows()

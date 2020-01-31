d88c84a0e5e844db2ab319e7c7d16643f90a3c7e
jsagithub <jsa_epe@hotmail.com>
2020-01-31 19:27:44 +0000

    grey filter video

--- get_started_with_video.py
+++ get_started_with_video.py
@@ -4,7 +4,8 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 

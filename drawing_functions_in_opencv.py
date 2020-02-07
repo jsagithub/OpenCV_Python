04d73643ad2372f1f83aa8d607f79b49a0e104e9
jsagithub <jsa_epe@hotmail.com>
2020-02-07 19:47:01 +0000

    text on image

--- drawing_functions_in_opencv.py
+++ drawing_functions_in_opencv.py
@@ -6,6 +6,9 @@ img = cv2.line(img, (0, 0), (255, 255), (255, 0, 0), 5)
 img = cv2.arrowedLine(img, (0, 255), (255, 255), (255, 0, 0), 5)
 
 img = cv2.rectangle(img, (384, 0), (510, 128), (0, 0, 255), 10)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+img = cv2.putText(img, 'Opencv', (10, 500), font, 4, (255, 255, 255), 1)
 cv2.imshow('image', img)
 
 cv2.waitKey(0)

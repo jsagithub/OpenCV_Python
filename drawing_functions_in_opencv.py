cf2588027cd4f04939f7d24a14eb377e61fbec8f
jsagithub <jsa_epe@hotmail.com>
2020-02-07 19:41:57 +0000

    draw  rectangle

--- drawing_functions_in_opencv.py
+++ drawing_functions_in_opencv.py
@@ -4,6 +4,8 @@ import cv2
 img = cv2.imread('lena.jpg', 1)
 img = cv2.line(img, (0, 0), (255, 255), (255, 0, 0), 5)
 img = cv2.arrowedLine(img, (0, 255), (255, 255), (255, 0, 0), 5)
+
+img = cv2.rectangle(img, (384, 0), (510, 128), (0, 0, 255), 10)
 cv2.imshow('image', img)
 
 cv2.waitKey(0)

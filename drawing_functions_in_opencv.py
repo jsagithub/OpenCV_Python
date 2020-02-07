d660339899ec3bf744ec6c4d2ad8dc74d2863f34
jsagithub <jsa_epe@hotmail.com>
2020-02-07 19:40:08 +0000

    add arrow line

--- drawing_functions_in_opencv.py
+++ drawing_functions_in_opencv.py
@@ -1,8 +1,9 @@
-import numpy as np 
+import numpy as np
 import cv2
 
-img = cv2.imread('lena.jpg',0)
-img = cv2.line(img,(0,0),(255,255), (255,0,0))
+img = cv2.imread('lena.jpg', 1)
+img = cv2.line(img, (0, 0), (255, 255), (255, 0, 0), 5)
+img = cv2.arrowedLine(img, (0, 255), (255, 255), (255, 0, 0), 5)
 cv2.imshow('image', img)
 
 cv2.waitKey(0)

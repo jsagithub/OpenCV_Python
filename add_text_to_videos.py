dfd0dfe06376f31851b2c1454b342b9d51b78165
jsagithub <jsa_epe@hotmail.com>
2020-02-11 19:20:20 +0000

    get and show width of camera screen addin text

--- add_text_to_videos.py
+++ add_text_to_videos.py
@@ -2,15 +2,20 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-#Set WIDTH
-cap.set(3, 1280)
-#SET HEIGHT
-cap.set(2, 720)
+# Set WIDTH
+#cap.set(3, 1280)
+# SET HEIGHT
+#cap.set(2, 720)
+
 while(cap.isOpened()):
     ret, frame = cap.read()
     if(ret == True):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', gray)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = 'Whidth' + str(cap.get(3))
+
+        cv2.putText(frame, text, (10, 50), font, 1,
+                    (0, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:

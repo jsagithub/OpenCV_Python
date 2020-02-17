1626343a2921b4c7b7261018670980ce0b10f083
jsagithub <jsa_epe@hotmail.com>
2020-02-17 14:09:45 +0000

    Draw conecting lines on image

--- mouse_event_opencv.py
+++ mouse_event_opencv.py
@@ -8,25 +8,17 @@ import cv2
 
 def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print (x, ', ', y)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        strX = str(x) + ', '+str(y)
-        cv2.putText(img, strX, (10, 500),
-                    font, 4, (255, 255, 255), 1)
+        cv2.circle(img,(x,y),3,(0,0,255), -1)
+        points.append((x,y))
+        if len(points)<=2:
+            cv2.line(img, points[-1],points[-2],(255,0,0), 5)
         cv2.imshow('image', img)
-    if event == cv2.EVENT_RBUTTONDOWN:
-        blue = img[y, x, 0]
-        green = img[y, x, 1]
-        red = img[y, x, 2]
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
-        cv2.putText(img, strBGR, (x, y), font, .5, (0, 255, 255), 2)
-        cv2.imshow('image', img)
+   
 
 
 img = np.zeros((512, 512, 3), np.uint8)
 cv2.imshow('image', img)
-
+points=[]
 cv2.setMouseCallback('image', click_event)
 
 cv2.waitKey(0)

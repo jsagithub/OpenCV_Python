6ed9bcbf6d0ffd0ced4c91f2e3b3683c5852b24f
jsagithub <jsa_epe@hotmail.com>
2020-02-27 15:47:15 +0000

    Binary THREsh

--- thresholding.py
+++ thresholding.py
@@ -3,6 +3,10 @@ import numpy as np
 
 img = cv.imread('gradient.png',0)
 
+_, th1=cv.threshold(img, 127, 255, cv.THESH_BINARY)
+
 cv.imshow("Image",img)
+cv.imshow("th1",th1)
+
 cv.waitKey(0)
 cv.destroyAllwindows()
